027e5a32bc2f0b5f88c288cb8dfb9e6480edb32e
luismoroco <lmorocoramos@gmail.com>
2023-05-17 10:45:10 -0500

    Generated by Makefile

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir data
+var=$1
+
+if [ -z "$var" ]; then
+  echo "A option is needed!"
+else
+  if [ "$var" = "s" ]; then
+    cd data 
+    echo "Configuring SMALL dataset"
+    wget https://files.grouplens.org/datasets/movielens/ml-latest-small.zip
+    unzip ml-latest-small.zip
+    rm ml-latest-small.zip
+    cd .. 
+    echo "Small Data Generated in ./data!"
+  elif [ "$var" = "f" ]; then
+    cd data
+    echo "Configuring FLL dataset"
+    wget https://files.grouplens.org/datasets/movielens/ml-latest.zip
+    unzip ml-latest.zip
+    rm ml-latest.zip
+    cd .. 
+    echo "Full Data Generated in ./data!"
+  else
+    echo "The var is not a option!"
+  fi 
+fi
---
+++ src/spark/app/main.py
@@ -0,0 +1,1 @@
+# spark classes and funcs
