bfecbd17691a9cda2f3acb0ae4c6cf5f66e844bc
lmoroco <lmorocoramos@gmail.com>
2023-05-18 20:01:17 -0500

    feat: :zap: adding ALS fore predictions

--- src/spark/app/dist.py
+++ src/spark/app/dist.py
@@ -15,11 +15,16 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 path = os.path.join(script_dir, 'ratings.csv')
 
 df = spark.read.csv(path, header=True, inferSchema=True)
-rdd = df.rdd.map(lambda row: (row[0], row[1], row[2])).cache()
+
+
+"""rdd = df.rdd.map(lambda row: (row[0], row[1], row[2])).cache()
 
 coordinates = rdd.map(lambda x: (x[0], x[1], x[2]))
-matrix = CoordinateMatrix(coordinates)
+matrix = CoordinateMatrix(coordinates)"""
 
-print(matrix)
+train, test = df.randomSplit([0.8, 0.2])
+
+recommerder = ALS(userCol='userId', ratingCol='rating', itemCol='movieId')
+recommerder.fit(train)
 
 spark.stop()
